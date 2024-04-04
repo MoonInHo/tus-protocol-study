0d91279c2d1fcf54585828c9ab5512b7cabfb476
MoonInHo <ansdlsgh55@gmail.com>
2024-04-04 19:57:04 +0900

    first commit

---
+++ src/main/java/com/example/tusprotocol/TusProtocolApplication.java
@@ -0,0 +1,13 @@
+package com.example.tusprotocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusProtocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TusProtocolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tusprotocol/TusProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tusprotocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TusProtocolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
